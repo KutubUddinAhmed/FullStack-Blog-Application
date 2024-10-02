08052406afccbeb076629750a077ba95eb416596
biswarup16 <biswarupdaslyf@gmail.com>
2024-10-02 22:55:39 +0530

    Fixed footer alignment

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -6,45 +6,60 @@ import { Button } from "./ui/button";
 
 function Footer() {
   return (
-    <footer className="my-3 h-[300px] p-1 ">
-      <section className="bg-black/85 text-white px-2 py-4 h-full rounded-lg flex flex-col space-y-20">
-        {/* Social media logos */}
-        <div className="social-media flex space-x-2 justify-end">
-          {social.map((logo: { images: string; slug: string }, i: number) => (
-            <Link key={i} href={`${logo.slug}`} target="_blank">
-              <div className="relative h-[26px] w-[28px] overflow-hidden rounded-full bg-white">
-                <Image
-                  src={logo.images}
-                  alt="social media logos"
-                  width={80}
-                  height={80}
-                  className="h-[26px] w-[28px] rounded-full mix-blend-multiply cursor-pointer"
+    <footer className="my-3 h-[300px] p-1 bg-black/85 mx-6 rounded-md">
+      <div className=" container mx-auto h-full p-5">
+        <section className="  text-white px-2 py-4 h-full rounded-lg flex items-center justify-center relative flex-col space-y-20">
+          {/* Social media logos */}
+          <div className=" flex justify-center flex-col">
+            <div className="social-media flex space-x-2 items-center justify-end absolute right-5 top-5">
+              {social.map(
+                (logo: { images: string; slug: string }, i: number) => (
+                  <Link key={i} href={`${logo.slug}`} target="_blank">
+                    <div className="relative h-[28px] w-[28px] overflow-hidden rounded-full bg-white">
+                      <Image
+                        src={logo.images}
+                        alt="social media logos"
+                        width={80}
+                        height={80}
+                        className="h-[28px] w-full rounded-full mix-blend-multiply cursor-pointer"
+                      />
+                    </div>
+                  </Link>
+                )
+              )}
+            </div>
+            {/* Contact section */}
+            <div className="flex items-center justify-center">
+              <div className="flex items-center mb-2 space-x-5">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="border p-[6px] rounded-md text-black w-full"
                 />
+                <Button className="p-[21px]">Submit</Button>
               </div>
-            </Link>
-          ))}
-        </div>
-        {/* Contact section */}
-        <div className="flex items-center justify-center">
-          <div className="flex items-center mb-2 space-x-16">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="border rounded p-2 text-black"
-            />
-            <Button className="p-[21px]">Submit</Button>
-          </div>
-        </div>
+            </div>
 
-        {/* Bottom conatiner */}
-        <div className="font-sans flex items-center sm:flex-row sm:items-center sm:justify-between text-balance text-sm justify-center space-y-4 sm:space-y-0 sm:space-x-6">
-          <span className="flex items-center text-balance">Logo and Text</span>
-          <span className="font-sans">
-            &copy; All rights reserved. {new Date().getFullYear()}.
-          </span>
-          <span>Made with love for readers</span>
-        </div>
-      </section>
+            {/* Bottom conatiner */}
+            {/* <div className="font-sans flex items-center sm:flex-row sm:items-center sm:justify-between text-balance text-sm justify-center space-y-4 sm:space-y-0 sm:space-x-6">
+            <p className="flex items-center text-balance justify-center">Logo and Text</p>
+            <p className="font-sans flex justify-center items-center">
+              &copy; All rights reserved. {new Date().getFullYear()}.
+            </p>
+            <p className="flex justify-center items-center">Made with love for readers</p>
+          </div> */}
+            <div className="absolute bottom-0 lg:bottom-5 w-full left-0 right-0">
+              <div className=" space-y-2  lg:space-y-0 lg:flex justify-between items-center ">
+                <p className="text-center">Logo and Text</p>
+                <p className="text-center">
+                  &copy; All rights reserved. {new Date().getFullYear()}.
+                </p>
+                <p className="text-center">Made with love for readers</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </footer>
   );
 }
--- frontend/components/multi_blog.tsx
+++ frontend/components/multi_blog.tsx
@@ -25,40 +25,37 @@ function Multi_blog() {
               },
               i: number
             ) => (
-              <Link href={`/blog/${blog.Slug}`}    key={i}>
-               <div
-             
-                className="card dark:border-none border rounded-lg shadow-lg p-4 mb-4 mt-1 bg-white dark:shadow dark:shadow-white"
-              >
-                <div className="py-2 px-1">
-                  <h2 className="text-2xl font-serif font-bold mb-2 text-gray-900 dark:text-black">
-                    {blog.Title}
-                  </h2>
-                  <p className="text-gray-700 dark:text-slate-700  truncate">
-                    {blog.Content}
-                  </p>
-                  <p className="text-gray-700 dark:text-slate-800 font-semibold font-sans mb-4">
-                    {blog.category}
-                  </p>
+              <Link href={`/blog/${blog.Slug}`} key={i}>
+                <div className="card dark:border-none border rounded-lg shadow-lg p-4 mb-4 mt-1 bg-white dark:shadow dark:shadow-white">
+                  <div className="py-2 px-1">
+                    <h2 className="text-2xl font-serif font-bold mb-2 text-gray-900 dark:text-black">
+                      {blog.Title}
+                    </h2>
+                    <p className="text-gray-700 dark:text-slate-700  truncate">
+                      {blog.Content}
+                    </p>
+                    <p className="text-gray-700 dark:text-slate-800 font-semibold font-sans mb-4">
+                      {blog.category}
+                    </p>
 
-                  <div className="details text-gray-500 dark:text-gray-900 text-sm flex items-center space-x-4">
-                    <div className="w-10 h-10 rounded-full border overflow-hidden">
-                      <Image
-                        src={blog.Image}
-                        alt="Author Image"
-                        width={40}
-                        height={60}
-                        className=" object-contain "
-                      />
-                    </div>
-                    <p>By {blog.Author}</p>
-                    <div className="flex space-x-1">
-                      <span>{blog.Date}</span>
-                      <span>{blog.Time}</span>
+                    <div className="details text-gray-500 dark:text-gray-900 text-sm flex items-center space-x-4">
+                      <div className="w-10 h-10 rounded-full border overflow-hidden">
+                        <Image
+                          src={blog.Image}
+                          alt="Author Image"
+                          width={40}
+                          height={60}
+                          className=" object-contain "
+                        />
+                      </div>
+                      <p>By {blog.Author}</p>
+                      <div className="flex space-x-1">
+                        <span>{blog.Date}</span>
+                        <span>{blog.Time}</span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
               </Link>
             )
           )}
@@ -69,53 +66,49 @@ function Multi_blog() {
             <span className="grid place-items-center text-3xl font-semibold mb-4">
               Latest Blog Author
             </span>
- <div className="max-h-[600px] overflow-y-auto scrollbar-hide overflow-x-hidden ">
-  {right_blogs.map(
-    (
-      rblogs: {
-        Title: string;
-        Author: string;
-        Image: string;
-        Date: string;
-        Time: string;
-        Slug: string;
-      },
-      i: number
-    ) => (
-      <Link href={`/blog/${rblogs.Slug}`} key={i}>
-        <div
-        
-        className="blog-card bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-[102%] border-2 border-black/20 dark:border-white/80 mx-3 my-1.5 min-h-[180px] py-3 grid place-content-center"
-      >
-        <div className="flex items-center mx-2 px-2 pl-5 space-x-2">
-          <div className="blog-image border-2 border-black/20 rounded-full">
-            <Image
-              src={rblogs.Image}
-              alt="Author's picture"
-              width={96} 
-              height={96}
-              className="w-[100px] p-2 h-[100px] rounded-full object-contain"
-            />
-          </div>
+            <div className="max-h-[600px] overflow-y-auto scrollbar-hide overflow-x-hidden ">
+              {right_blogs.map(
+                (
+                  rblogs: {
+                    Title: string;
+                    Author: string;
+                    Image: string;
+                    Date: string;
+                    Time: string;
+                    Slug: string;
+                  },
+                  i: number
+                ) => (
+                  <Link href={`/blog/${rblogs.Slug}`} key={i}>
+                    <div className="blog-card bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-[102%] border-2 border-black/20 dark:border-white/80 mx-3 my-1.5 min-h-[180px] py-3 grid place-content-center">
+                      <div className="flex items-center mx-2 px-2 pl-5 space-x-2">
+                        <div className="blog-image border-2 border-black/20 rounded-full">
+                          <Image
+                            src={rblogs.Image}
+                            alt="Author's picture"
+                            width={96}
+                            height={96}
+                            className="w-[100px] p-2 h-[100px] rounded-full object-contain"
+                          />
+                        </div>
 
-          <div className="blog-content p-4">
-            <p className="blog-author text-black font-sans font-semibold">
-              By {rblogs.Author}
-            </p>
-            <h2 className="blog-title text-2xl font-bold mb-2 text-black font-serif">
-              {rblogs.Title}
-            </h2>
-            <p className="blog-date text-gray-500 dark:text-gray-400 text-sm mt-1">
-              {rblogs.Date} - {rblogs.Time}
-            </p>
-          </div>
-        </div>
-      </div>
-      </Link>
-    )
-  )}
-</div>
-
+                        <div className="blog-content p-4">
+                          <p className="blog-author text-black font-sans font-semibold">
+                            By {rblogs.Author}
+                          </p>
+                          <h2 className="blog-title text-2xl font-bold mb-2 text-black font-serif">
+                            {rblogs.Title}
+                          </h2>
+                          <p className="blog-date text-gray-500 dark:text-gray-400 text-sm mt-1">
+                            {rblogs.Date} - {rblogs.Time}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                )
+              )}
+            </div>
           </div>
         </div>
       </div>
