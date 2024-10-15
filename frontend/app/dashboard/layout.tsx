75af708a36f8a9db51404776eb9d6273268c7c6b
KutubUddinAhmed <kutubahmed25012@gmail.com>
2024-10-15 19:47:26 +0530

    Dashboardlayout not working properly

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,9 +1,17 @@
 import Dashboard from "@/components/Dashboard"
+import Context from "@/context/context"
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <section><Dashboard /></section>
+    return (
+        <Context>
+        <section>
+            This is Dashboard
+            {children}
+            </section>
+            </Context>
+            )
 }
